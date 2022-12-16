74779be026e4ee6c66d6cf4691add3c0501e10ae
sujin9335 <sujin93359335@gmail.com>
2022-12-17 07:41:23 +0900

    12.17

--- 13_컬렉션/src/com/collection/controller/AnimalController.java
+++ 13_컬렉션/src/com/collection/controller/AnimalController.java
@@ -1,6 +1,8 @@
 package com.collection.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import com.collection.model.vo.Animal;
 
@@ -10,12 +12,51 @@ public class AnimalController {
 		ArrayList list=new ArrayList();
 		list.add(new Animal("뽀삐", 5.3, 10, "강아지"));
 		list.add(new Animal("야옹이", 4.2, 5, "고양이"));
-		Animal c=new Animal("킥킥이", 15.3, 8, "원숭이");
-		Animal d=new Animal("하하", 200.3, 9, "하마");
+		list.add(new Animal("킥킥이", 15.3, 8, "원숭이"));
+		list.add(new Animal("하하", 200.3, 9, "하마"));
+		
+		System.out.println("1");
+//		animalPrint(list);
+		System.out.println(list.toString());
+		
+		
+		System.out.println("2");
+		for(int i=0; i<list.size(); i++) {
+			Animal a=(Animal)list.get(i);
+			System.out.println(a.getName()+" "+a.getAge()+" "+a.getWeight());
+		}
+		
+		System.out.println("3");
+		for(int i=0; i<list.size(); i++) {
+			Animal a=(Animal)list.get(i);
+			if(a.getWeight()>=10) {
+				System.out.println(a.getName()+" "+a.getType()+" "+a.getWeight());
+			}
+		}
+		
+		System.out.println("4");
+		for(int i=0; i<list.size(); i++) {
+			Animal a=(Animal)list.get(i);
+			if(a.getAge()>=5) {
+				System.out.println(a);
+			}
+		}
+		
+		System.out.println("5");
+		for(int i=0; i<list.size(); i++) {
+			Animal a=(Animal)list.get(i);
+			a.setAge(a.getAge()+1);
+			System.out.println(a);
+		}
 		
 	
 	}
 	
+	public static void animalPrint(List animal) {
+		for(int i=0; i<animal.size(); i++) {
+			System.out.println(animal.get(i));
+		}
+	}
 	
 	
 }
--- 13_컬렉션/src/com/collection/run/AnimalMain.java
+++ 13_컬렉션/src/com/collection/run/AnimalMain.java
@@ -1,8 +1,12 @@
 package com.collection.run;
 
+import com.collection.controller.AnimalController;
+
 public class AnimalMain {
 
 	public static void main(String[] args) {
+		AnimalController ac=new AnimalController();
+		ac.inputAnimal();
 		
 	}
 }
